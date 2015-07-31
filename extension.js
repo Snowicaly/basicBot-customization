9d06df3fa7b57552b2c5b968b7b7927b546952f6
Snowicaly <animetears@live.se>
2015-07-31 19:39:51 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -96,7 +96,7 @@
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
-        motdEnabled: false,
+        motdEnabled: true,
         motdInterval: 5,
         motd: "Fancy seeing you here.",
         filterChat: true,
