ac380a00fc9950c980518b5c36ff3a7d0fb9d4bf
Snowicaly <animetears@live.se>
2015-07-31 19:28:47 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -120,6 +120,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Snowicaly/basicBot/master/basicBot.js", extend);
 
 }).call(this);
